1c16eab44c20a483268bb0f92edf97dca8a2d5c3
Lucas Sotero <lucassoteroo14@gmail.com>
2025-11-08 00:30:26 -0300

    Atualização para Angular 19 concluída

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.scss'],
+    standalone: false
 })
 export class NavComponent implements OnInit {
   public valor: boolean = false;
